83dc883a982357f0187ba906969d8d7d6d35b67e
kamy1997 <kamykechir@gmail.com>
2022-03-02 13:01:49 +0100

    Netflix back-end end

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,9 @@ const usersRouter = require('./users.route');
 const checkoutRouter = require('./checkout.route');
 const webHooksRouter = require('./webhooks.route');
 const orderRouter = require('./order.route');
-const productRouter = require('./product.route')
+const productRouter = require('./product.route');
 ;
 router.use('/users/', usersRouter);
-router.use('/checkout',checkoutRouter);
-router.use('/webhooks',webHooksRouter);
-router.use('/order',orderRouter);
 router.use('/products/', productRouter);
 
 module.exports = router;
